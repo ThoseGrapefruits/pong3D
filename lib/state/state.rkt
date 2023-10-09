ea1bbaf29fa4a6ba02140fa023b2b5906c333bc2
Logan Moore <lmooore@gmail.com>
2023-10-09 11:30:08 -0500

    Move state structs & syntax out of main file

---
+++ lib/state/state.rkt
@@ -0,0 +1,54 @@
+#lang typed/racket/base
+
+(require
+  pict3d)
+
+(struct Ball
+  ([dir : Dir]
+   [pos : Pos]))
+
+(struct Opponent
+  ([y : Flonum ]))
+
+(struct Player
+  ([lives : Nonnegative-Integer]
+   [score : Nonnegative-Integer]
+   [score-multiplier : Nonnegative-Flonum]
+   [y : Flonum]))
+
+(struct State
+  ([dt   : Flonum]
+   [n    : Natural]
+   [pressed : (Setof String)]
+   [t    : Flonum])
+  #:transparent)
+
+(struct State-Game-Over State
+  ([end-state : State-Play])
+  #:transparent)
+
+(struct State-Main-Menu State
+  ()
+  #:transparent)
+
+(struct State-Paused State
+  ; hold a copy of the last play state for when the game resumes
+  ([resume-state : State-Play])
+  #:transparent)
+
+(struct State-Play State
+  ([ball : Ball]
+   [opponent : Opponent]
+   [player : Player]
+   [start-t : Flonum])
+  #:transparent)
+
+(provide
+ (struct-out Ball)
+ (struct-out Opponent)
+ (struct-out Player)
+ (struct-out State)
+ (struct-out State-Game-Over)
+ (struct-out State-Main-Menu)
+ (struct-out State-Paused)
+ (struct-out State-Play))
---
+++ lib/state/syntax.rkt
@@ -0,0 +1,32 @@
+#lang typed/racket
+
+(require "./state.rkt")
+
+(provide (all-defined-out))
+
+; [distant, maniacal laughter]
+; https://stackoverflow.com/a/77208704/2883258
+;
+; This horrible macro expansion thing lets us get around the fact that
+; struct-copy doesn't have much runtime flexibility with choosing what child
+; struct to use when copying, while avoiding repeating the same cond statement
+; in a bunch of different update methods.
+(define-syntax-rule (base-state-update s [t #:parent State v] ...)
+  (cond [(State-Game-Over? s)
+         (struct-copy State-Game-Over s [t #:parent State v] ...)]
+        [(State-Main-Menu? s)
+         (struct-copy State-Main-Menu s [t #:parent State v] ...)]
+        [(State-Paused? s)
+         (struct-copy State-Paused s [t #:parent State v] ...)]
+        [(State-Play? s)
+         (struct-copy State-Play s [t #:parent State v] ...)]
+        [else (error (~s "Invalid state: " s))]))
+
+; This other horrible macro syntax expansion thing lets us transition between
+; states more easily, copying the fields stored on the base State struct.
+(define-syntax-rule (state-transition S s field ...)
+  (S (State-dt s)
+     (State-n s)
+     (State-pressed s)
+     (State-t s)
+     field ...))
