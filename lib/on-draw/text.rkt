f647f468b3286d26904150660e257dd977810502
Logan Moore <lmooore@gmail.com>
2024-02-19 13:26:06 -0600

    `for/fold`ify text stuff

--- lib/on-draw/text.rkt
+++ lib/on-draw/text.rkt
@@ -1,18 +1,21 @@
 #lang typed/racket
 
-(require parser-tools/lex
-         pict3d)
+(require pict3d)
 
 (require (prefix-in font: "font.rkt"))
 
 (provide text)
 
+; TODO it would be nice to use something like parser-tools/lex to 
+; pull out multi-character things and allow for e.g. ligatures.
 (: text-map : (Immutable-HashTable Char font:Char-3D))
 (define text-map
   (make-immutable-hash
    (list
     (cons #\a font:a)
     (cons #\space font:ws-space))))
+
+; TYPES ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (define-struct Word-3D
   ([width : Flonum]
@@ -33,34 +36,77 @@
   (define width : Flonum (foldl + 0.0 (map font:Char-3D-width chars-mapped)))
   (Word-3D width chars-mapped))
 
-(: text : String [#:wrap Flonum] -> Pict3D)
-(define (text s #:wrap [wrap 40.0])
+(: WORD-SPACE Word-3D)
+(define WORD-SPACE (Word-3D-from-chars (list font:ws-space)))
+
+(: WORD-SPACE/WIDTH Flonum)
+(define WORD-SPACE/WIDTH (Word-3D-width WORD-SPACE))
+
+(: text : String [#:line-height Flonum] [#:wrap Flonum] -> Pict3D)
+(define (text s
+              #:line-height [line-height 1.2]
+              #:wrap [wrap 40.0])
   (define words (string-split s))
   (define words-rendered (map Word-3D-from-string words))
-  (define lines-reversed (foldl (line-maker wrap) '() words-rendered))
-  (define lines (map reverse-line (reverse lines-reversed)))
-  ; TODO render things in correct positions
+  (define lines (make-lines words-rendered #:wrap wrap))
   (combine
-   (for*/list : (Listof Pict3D) ([line lines])
-     (combine
-      (for*/list : (Listof Pict3D) ([ word line ])
-      (combine
-       (for*/list : (Listof Pict3D) ([ char (Word-3D-chars word) ])
-       (font:Char-3D-drawn char))))))))
+   (for/list : (Listof Pict3D)
+     ([i (range 0 (length lines))]
+      [line lines])
+     (define line-y (* i line-height))
+     (: line-rendered (Listof Pict3D))
+     (define-values (_ line-rendered)
+       (for/fold : (Values Flonum (Listof Pict3D))
+         ([word-x : Flonum 0.0]
+          [out empty-list/Pict3D])
+         ([word line])
+         (define-values (word-x-new chars-rendered)
+           (for/fold : (Values Flonum (Listof Pict3D))
+             ([char-x word-x]
+              [out empty-list/Pict3D])
+             ([char (Word-3D-chars word)])
+             (define char-positioned
+               (transform
+                (font:Char-3D-drawn char)
+                (affine-compose
+                 (move-x char-x)
+                 (move-y line-y))))
+             (values (+ char-x (font:Char-3D-width char))
+                     (cons char-positioned out))))
+         (values word-x-new
+                 (cons (combine chars-rendered) out))))
+     (combine line-rendered))))
 
-(: line-maker : Flonum -> (-> Word-3D (Listof (Listof Word-3D)) (Listof (Listof Word-3D)))) 
-(define (line-maker [wrap 40.0])
-  (lambda (word lines)
-    (define word-width (Word-3D-width word))
-    (define line-length (foldl + word-width (map Word-3D-width (first lines))))
-    (if (> line-length wrap)
-        (cons
-          (list word)
-          lines)
-        (cons
-          (cons word (first lines))
-          (rest lines)))))
+(: make-lines : (Listof Word-3D) [#:wrap Flonum] -> (Listof (Listof Word-3D)))
+(define (make-lines words #:wrap [wrap 40.0])
+  (map reverse-line (reverse (make-lines-reversed words #:wrap wrap))))
+
+(: make-lines-reversed : (Listof Word-3D) [#:wrap Flonum] -> (Listof (Listof Word-3D)))
+(define (make-lines-reversed words #:wrap [wrap 40.0])
+  (define-values (_ lines)
+    (for/fold : (Values Flonum (Listof (Listof Word-3D))) ([line-length 0.0]
+               [lines empty-list-list/Word-3D])
+              ([word words])
+      (define word-width (Word-3D-width word))
+      (if (> (+ line-length word-width) wrap)
+          (values 0.0
+                  (cons
+                   (list word)
+                   lines))
+          (values (+ line-length word-width WORD-SPACE/WIDTH)
+                  (cons
+                   (cons word (cons WORD-SPACE (car lines)))
+                   (cdr lines))))))
+  lines)
+
+; UTILS ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ; typed racket chokes without this and I couldn't figure out why
 (: reverse-line : (Listof Word-3D) -> (Listof Word-3D))
 (define (reverse-line l) (reverse l))
+
+(: empty-list/Pict3D (Listof Pict3D))
+(define empty-list/Pict3D empty)
+
+(: empty-list-list/Word-3D (Listof (Listof Word-3D)))
+(define empty-list-list/Word-3D (list empty))
