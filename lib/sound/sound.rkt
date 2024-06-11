fb09599c8544d777e090eaa4d5408ae2bc7cc0d2
Logan Moore <lmooore@gmail.com>
2024-06-10 22:23:31 -0500

    Sound refactor, change volume preparation

--- lib/sound/sound.rkt
+++ lib/sound/sound.rkt
@@ -3,35 +3,42 @@
 (require racket/file
          racket/list
          racket/math
-         typed/racket/gui/base)
+         typed/racket/gui/base
+         "../preferences/preferences.rkt")
 
 (module wrapper racket/base
-  (require (prefix-in rs: rsound))
+  (require rsound)
   (provide
-   rs:andplay
-   rs:default-sample-rate
-   rs:ding
-   rs:make-ding
-   rs:play
-   rs:rs-overlay
-   rs:rs-append*
-   rs:rs-write
-   rs:rsound
-   rs:rsound?
-   rs:rs-scale
-   rs:synth-note
-   rs:stop))
+   (prefix-out rs: andplay)
+   (prefix-out rs: default-sample-rate)
+   (struct-out rsound)
+   (prefix-out rs: ding)
+   (prefix-out rs: make-ding)
+   (prefix-out rs: play)
+   (rename-out [rs-overlay rs:overlay]
+               [rs-append* rs:append*]
+               [rs-write   rs:write]
+               [rs-scale   rs:scale])
+   (prefix-out rs: synth-note)
+   (prefix-out rs: stop)))
+
+(: volume-master : Flonum)
+(define volume-master (get-preference-pong3d-flonum 'volume-master (λ () 0.5)))
 
 (require/typed
  'wrapper
- [#:opaque RSound rs:rsound?]
+ [#:struct rsound ([data : (Vectorof Real)]
+                      [start : Nonnegative-Real]
+                      [stop : Nonnegative-Real]
+                      [sample-rate : Nonnegative-Real])
+                      #:type-name RSound]
  [rs:default-sample-rate (-> Positive-Real)]
  [rs:ding RSound]
  [rs:make-ding (-> Integer RSound)]
- [rs:rs-append* (-> (Sequenceof RSound) RSound)]
- [rs:rs-overlay (-> RSound RSound RSound)]
- [rs:rs-write (-> RSound Path Void)]
- [rs:rs-scale (-> Real RSound RSound)]
+ [rs:append* (-> (Sequenceof RSound) RSound)]
+ [rs:overlay (-> RSound RSound RSound)]
+ [rs:write (-> RSound Path Void)]
+ [rs:scale (-> Real RSound RSound)]
  [rs:stop (-> Void)]
  [rs:synth-note (-> String Number Natural Natural RSound)])
 
@@ -40,7 +47,6 @@
  rs-play-random
  rs-stop
 
- SOUND-DING
  SOUND-ENDGAME
  SOUND-MUSIC
  SOUND-SCORE-ON-OPPONENT
@@ -50,26 +56,39 @@
  SOUNDS-BALL-BOUNCE-PLAYER
  SOUNDS-BALL-BOUNCE-WALL)
 
+(struct PongSound ([name : Symbol]
+                   [sound : RSound]))
+
+(define-type PongSounds (Listof PongSound))
+
 ;; PLAY/STOP
 
-(: path-cache (Mutable-HashTable RSound Path))
+(: path-cache (Mutable-HashTable String Path))
 (define path-cache (make-hash))
 
-(: rs-play (->* (RSound) (Boolean) Thread))
+(: rs-play (->* (PongSound) (Boolean) Thread))
 (define (rs-play sound [loop #f])
   ; TODO would be good to pull this over but it's an internal function in rsound
   ; (rs:check-below-threshold sound 2.0)
-  (define cached-path (hash-ref path-cache sound #f))
-  (define path (or cached-path (make-temporary-file "pong3d-sound-~a.wav")))
-  (hash-set! path-cache sound path)
-  (rs:rs-write sound path)
+  (define (get-path)
+    (define volume volume-master)
+    (define cache-key (format "~s-~s" (symbol->string (PongSound-name sound)) volume))
+    (define cached-path (hash-ref path-cache cache-key #f))
+    (define path (or cached-path
+                     (make-temporary-file (format "pong3d-sound-~s-~~a.wav" cache-key))))
+    (hash-set! path-cache cache-key path)
+    (rs:write (rs:scale volume (PongSound-sound sound)) path)
+    path)
+
   (: play (-> Boolean))
-  (define play (if loop
-                   (λ () (play-sound path #f) (play))
-                   (λ () (play-sound path #f))))
+  (define play
+    (if loop
+        (λ () (play-sound (get-path) #f) (play))
+        (λ () (play-sound (get-path) #f))))
+
   (thread play))
 
-(: rs-play-random (->* ((Listof RSound)) (Boolean) Thread))
+(: rs-play-random (->* (PongSounds) (Boolean) Thread))
 (define (rs-play-random sounds [loop #f])
   (define sound (list-ref sounds (random 0 (length sounds))))
   (rs-play sound loop))
@@ -84,9 +103,6 @@
 
 (: dur Positive-Real)
 (define dur (max 0.01 (/ 60 bpm)))
-
-(: SOUND-DING RSound)
-(define SOUND-DING rs:ding)
 
 ; Frequencies based on note function w/ middle C @ 440Hz
 ; y = 440 * 2^(x / 12)
@@ -112,73 +128,82 @@
 (define (note-to-frequency n)
   (exact-round (* 440 (expt 2 (/ (exact->inexact n) 12)))))
 
-(: SOUNDS-BALL-BOUNCE-OPPONENT (Listof RSound))
+(: SOUNDS-BALL-BOUNCE-OPPONENT PongSounds)
 (define SOUNDS-BALL-BOUNCE-OPPONENT
-  (for/list : (Listof RSound)
+  (for/list : PongSounds
     ([n (in-range -5 5)])
-    (rs:rs-scale 0.5 (rs:make-ding (+ (note-to-frequency -1) (* n 1))))))
+    (PongSound 'ball-bounce-player
+               (rs:make-ding (+ (note-to-frequency -1) (* n 1))))))
 
-(: SOUNDS-BALL-BOUNCE-PLAYER (Listof RSound))
+(: SOUNDS-BALL-BOUNCE-PLAYER PongSounds)
 (define SOUNDS-BALL-BOUNCE-PLAYER
-  (for/list : (Listof RSound)
+  (for/list : PongSounds
     ([n (in-range -5 5)])
-    (rs:rs-scale 0.5 (rs:make-ding (+ (note-to-frequency 3) (* n 1))))))
+    (PongSound 'ball-bounce-player
+               (rs:make-ding (+ (note-to-frequency 3) (* n 1))))))
 
-(: SOUNDS-BALL-BOUNCE-WALL (Listof RSound))
+(: SOUNDS-BALL-BOUNCE-WALL PongSounds)
 (define SOUNDS-BALL-BOUNCE-WALL
-(for/list : (Listof RSound)
+(for/list : PongSounds
   ([n (in-range -5 5)])
-  (rs:rs-scale 0.5 (rs:make-ding (+ (note-to-frequency 5) (* n 1))))))
+  (PongSound 'ball-bounce-wall
+             (rs:make-ding (+ (note-to-frequency 5) (* n 1))))))
 
-(: SOUND-ENDGAME RSound)
+(: SOUND-ENDGAME PongSound)
 (define SOUND-ENDGAME
-  (rs:rs-append*
-   (for*/list : (Listof RSound) ([note-dur (in-list '((8 0.5) (7 0.5) (1 0.5) (3 1)))])
-     (define note (first note-dur))
-     (define dur (second note-dur))
-     (rs:synth-note "vgame" 49 (+ 52 note)
-                    (exact-round
-                     (* (rs:default-sample-rate) dur))))))
+  (PongSound 'endgame
+             (rs:append*
+              (for*/list : (Listof RSound)
+                ([note-dur (in-list '((8 0.5) (7 0.5) (1 0.5) (3 1)))])
+                (define note (first note-dur))
+                (define dur (second note-dur))
+                (rs:synth-note "vgame" 49 (+ 52 note)
+                               (exact-round
+                                (* (rs:default-sample-rate) dur)))))))
 
-(: SOUND-MUSIC RSound)
+(: SOUND-MUSIC PongSound)
 (define SOUND-MUSIC
-  (rs:rs-overlay
-   (rs:rs-append*
-    (for*/list : (Listof RSound) ([i (in-range 20)]
-                                  [j (in-list '(0 2 3 7))])
-      (rs:synth-note "vgame" 49 (+ 60 j)
-                     (exact-round
-                      (* (rs:default-sample-rate) (/ dur 4))))))
-   (rs:rs-append*
-    (for*/list : (Listof RSound) ([i (in-range 5)]
-                                  [j (in-list '(8 3 4 3))])
-      (rs:synth-note "vgame" 49 (+ 52 j)
-                     (exact-round
-                      (* (rs:default-sample-rate) dur)))))))
+  (PongSound 'music
+             (rs:overlay
+              (rs:append*
+               (for*/list : (Listof RSound) ([i (in-range 20)]
+                                             [j (in-list '(0 2 3 7))])
+                 (rs:synth-note "vgame" 49 (+ 60 j)
+                                (exact-round
+                                 (* (rs:default-sample-rate) (/ dur 4))))))
+              (rs:append*
+               (for*/list : (Listof RSound) ([i (in-range 5)]
+                                             [j (in-list '(8 3 4 3))])
+                 (rs:synth-note "vgame" 49 (+ 52 j)
+                                (exact-round
+                                 (* (rs:default-sample-rate) dur))))))))
 
-(: SOUND-SCORE-ON-OPPONENT RSound)
+(: SOUND-SCORE-ON-OPPONENT PongSound)
 (define SOUND-SCORE-ON-OPPONENT
-  (rs:rs-append*
-   (for*/list : (Listof RSound)
-     ([j (in-list '(2 3 7))])
-     (rs:synth-note "vgame" 49 (+ 68 j)
-                    (exact-round
-                     (* (rs:default-sample-rate) (/ dur 4)))))))
+  (PongSound 'score-on-opponent
+             (rs:append*
+              (for*/list : (Listof RSound)
+                ([j (in-list '(2 3 7))])
+                (rs:synth-note "vgame" 49 (+ 68 j)
+                               (exact-round
+                                (* (rs:default-sample-rate) (/ dur 4))))))))
 
-(: SOUND-SCORE-ON-PLAYER RSound)
+(: SOUND-SCORE-ON-PLAYER PongSound)
 (define SOUND-SCORE-ON-PLAYER
-  (rs:rs-append*
-   (for*/list : (Listof RSound)
-     ([j (in-list '(7 3 2))])
-     (rs:synth-note "vgame" 49 (+ 52 j)
-                    (exact-round
-                     (* (rs:default-sample-rate) (/ dur 4)))))))
+  (PongSound 'score-on-player
+             (rs:append*
+              (for*/list : (Listof RSound)
+                ([j (in-list '(7 3 2))])
+                (rs:synth-note "vgame" 49 (+ 52 j)
+                               (exact-round
+                                (* (rs:default-sample-rate) (/ dur 4))))))))
 
-(: SOUND-STARTUP RSound)
+(: SOUND-STARTUP PongSound)
 (define SOUND-STARTUP
-  (rs:rs-append*
-   (for*/list : (Listof RSound)
-     ([j (in-list '(2 3 7 6))])
-     (rs:synth-note "vgame" 49 (+ 68 j)
-                    (exact-round
-                     (* (rs:default-sample-rate) (/ dur 2)))))))
+  (PongSound 'startup
+             (rs:append*
+              (for*/list : (Listof RSound)
+                ([j (in-list '(2 3 7 6))])
+                (rs:synth-note "vgame" 49 (+ 68 j)
+                               (exact-round
+                                (* (rs:default-sample-rate) (/ dur 2))))))))
